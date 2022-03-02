b0dc023fa6fc2795735742f696e6e18819d864bc
Ivan Reynes <ivan.reynes@outlook.com>
2022-03-02 13:19:05 +0800

    Added new files

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ivan Reynes</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <header>
+    <div class="menu-toggler">
+      <div class="bar half start"></div>
+      <div class="bar"></div>
+      <div class="bar half end"></div>
+    </div>
+    <nav class="top-nav">
+        <ul class="nav-list">
+            <li>
+              <a href="index.php" class="nav-link">Home</a>
+            </li>
+            <li>
+              <a href="#about" class="nav-link">About</a>
+            </li>
+            <li>
+              <a href="#services" class="nav-link">Habits</a>
+            </li>
+            <li>
+              <a href="#portfolio" class="nav-link">Posts</a>
+            </li>
+            <li>
+              <a href="#experince" class="nav-link">Experince</a>
+            </li>
+            <li>
+              <a href="#contact" class="nav-link">Contact</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="landing-text">
+      <h1> Ivan Reynes</h1>
+      <h6>UX/UI/IT | Web Designer and Web Developer</h6>
+    </div>
+  </header>
+
+    <section class="about" id="about">
+      <div class="container">
+        <div class="profile-img" data-aos="fade-right" data-aos-delay="300">
+          <img src="Images/Capture01.PNG" alt="">
+        </div>
+        <div class="about-details"  data-aos="fade-left" data-aos-delay="900">
+          <div class="about-heading">
+            <h1>About</h1>
+            <h6>Myself</h6>
+          </div>
+          <p style="text-align:justify">
+            "It's not how good you are, it's how good you want to be"  <br> - Paul Arden <br> <br>
+            Old but a very gold quote from the world's best selling book. It taught me to be positive out of everything in life. That
+            no matter how tough, how down, how dark you've been .. your desire of what you want to be is what matters. <br>
+           Well, I have encountered many things that tested my capability of being good yet I didn't let vengeance control over myself.
+           I've been rejected hundred times, I've been to the point that no one believes in me, to the point where the only thing I can rely is my
+           own shodow but those times made me realize how good underdog was, how I converted negativity into an inspiration.
+           With wittiness and knowledge on things I desire to achieve in life much like how Mark Zuckerberg pursued his FACEBOOK.
+          </p>
+          <div class="social-media">
+            <ul class="nav-list">
+              <li>
+                  <a href="https://www.facebook.com/skrinavz/" class="icon-link">
+                    <i class="fab fa-facebook-square"></i>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" class="icon-link">
+                    <i class="fab fa-twitter-square"></i>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" class="icon-link">
+                    <i class="fab fa-dribble-square"></i>
+                  </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="services" id="services">
+      <div class="container">
+        <div class="section-heading">
+            <h1>HABITS</h1>
+            <h6>My best interests in life</h6>
+        </div>
+        <div class="my-skills">
+            <div class="skill"  data-aos="fade-in" data-aos-delay="300">
+              <div class="icon-container">
+                <i class="fa fa-book"></i>
+              </div>
+              <h1>Reading</h1>
+              <p>
+                I always spent time reading books.
+                It made me relax from the issues society is facing. I always believe that the more I read, the more things I knew, and I am aware of.
+              </p>
+            </div>
+
+            <div class="skill" data-aos="fade-in" data-aos-delay="600">
+              <div class="icon-container">
+                <i class="fa fa-users"></i>
+              </div>
+              <h1>Meeting People</h1>
+              <p>
+                I totally love meeting new people that have a great vision in life.
+                The more I am closer to them, the higher the probability they might influence me in learning new things.
+              </p>
+            </div>
+
+
+            <div class="skill" data-aos="fade-in" data-aos-delay="900">
+              <div class="icon-container">
+                <i class="fas fa-chart-bar"></i>
+              </div>
+              <h1>Skills</h1>
+              <p>
+                I took work seriously until I eventually develop my own style and technique.
+                I love to do what I think is great.
+              </p>
+            </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="portfolio" id="portfolio">
+        <div class="container">
+          <div class="section-heading">
+            <h1>My Current Posts</h1>
+            <h6>Enjoy viewing some of my latest and interesting posts</h6>
+          </div>
+          <div class="portfolio-item">
+                <div class="portfolio-img has-margin-right" data-aos="fade-in" data-aos-delay="300">
+                  <img src="Images/InsideBill.jpg" alt="Bill Gates">
+                </div>
+                <div class="portfolio-description" data-aos="fade-left" data-aos-delay="600">
+                  <h6>June 28, 2020</h6>
+                  <h1>Inside Bill's Brain</h1>
+                  <p>
+                  This is a NETFLIX Docu-Series. I love watching this movie because it is full of knowledge and a very inspirational
+                  one. Even if he gets older, he never stops learning. He literally changed the world with his
+                  genius mindset.
+                  </p>
+                  <a href="https://www.youtube.com/watch?v=aCv29JKmHNY" class="cta">View Details</a>
+                </div>
+          </div>
+          <div class="portfolio-item">
+            <div class="portfolio-description has-margin-right" data-aos="fade-right" data-aos-delay="900">
+              <h6>March 13, 2018</h6>
+              <h1>Family Bonding</h1>
+              <p>
+                At Plantation Bay Resort, Mactan Cebu. The more I aged, the more I realize how tight our bond is. Out of millions, ours is my favorite.
+              </p>
+              <a href="https://www.facebook.com/skrinavz/" class="cta">View Details</a>
+            </div>
+            <div class="portfolio-img" data-aos="fade-left" data-aos-delay="1200">
+              <img src="Images/family.jpg" alt="">
+            </div>
+          </div>
+          <div class="portfolio-item">
+            <div class="portfolio-img has-margin-right" data-aos="fade-right" data-aos-delay="1500">
+              <img src="Images/fam.jpg" alt="">
+            </div>
+            <div class="portfolio-description" data-aos="fade-left" data-aos-delay="1800">
+              <h6>February 05, 2018</h6>
+              <h1>Chillin'!</h1>
+              <p>
+                Alcoholic you may call, but it is the best thing that we inherited from our ancestors 😅. Chillin' times
+                together with my family and circle of friends. Such a memorable moment though.
+              </p>
+              <a href="https://www.facebook.com/skrinavz/" class="cta">View Details</a>
+            </div>
+          </div>
+        </div>
+    </section>
+
+    <section class="experince" id="experince">
+      <div class="container">
+        <div class="section-heading">
+          <h1>Work Experience</h1>
+        </div>
+        <div class="timeline">
+          <ul>
+            <li class="date" data-date="2019 - Present">
+              <h1>Startlinks</h1>
+              <p>
+               Web design & Development
+              </p>
+            </li>
+            <li class="date" data-date="2017 - 2018">
+              <h1>MLhuillier Financial Services INC.</h1>
+              <p>
+                Branch Teller.
+              </p>
+            </li>
+            <li class="date" data-date="2016 - 2017">
+              <h1>SM Savemore</h1>
+              <p>
+                Costumer Service.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    <section class="contact" id="contact">
+      <div class="container">
+        <div class="section-heading">
+          <h1>Contact</h1>
+          <h6>LET US WORK TOGETHER</h6>
+        </div>
+        <form action="">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" placeholder="Enter your name..." required>
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email..." required>
+          <label for="service">Services:</label>
+          <select name="services" id="service">
+            <option value="">Web Design</option>
+            <option value="">Web Development</option>
+            <option value="">Web Design/Development</option>
+          </select>
+            <label for="subject">Subject:</label>
+            <textarea name="subject" id="subject" rows="10" cols="10"></textarea>
+            <input type="submit" onclick="myPayment()"  value="Submit">
+        </form>
+      </div>
+    </section>
+    <footer class="copyright">
+      <div class="up" id="up">
+        <i class="fas fa-chevron-up"></i>
+      </div>
+        <p>&copy; 2020 Ivan Reynes</p>
+    </footer>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script src="main.js"></script>
+</body>
+</html>
